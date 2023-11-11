6ba3d1e62fd6bb6e8404b02851e2bb9f68bf6a5e
HuberAdrian <huberadrian@web.de>
2023-11-11 18:48:23 +0100

    new

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,3 +11,25 @@ export const config = {
   matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
 };
 
+
+/* other method:
+import { NextResponse } from "next/server";
+ 
+
+import { authMiddleware } from "@clerk/nextjs";
+ 
+// run console.log on every request
+export default authMiddleware({
+    beforeAuth: () => {
+        console.log("beforeAuth");
+    }
+});
+
+ 
+// Stop Middleware running on static files
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+*/
+
+
