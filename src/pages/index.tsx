5686be11fbdce9377e32584db5a5b27c2dd1bb3b
HuberAdrian <huberadrian@web.de>
2023-12-12 19:29:08 +0100

    day component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,6 +42,47 @@ export default function Home() {
   );
 }
 
-// how to change color of text in list item using tailwindcss
 
-// <li className="text-[#2e026d]" key={post.id}>
+const Day = (/*{ meals }*/) => {
+  const meals = [
+    {
+      time: "8:00",
+      name: "Breakfast",
+      image: "https://images.unsplash.com/photo-1581091019743-7aaffed1b6d6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJlYWtmYXN0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+      ingredients: "2 eggs, 2 slices of bread, 1 cup of milk",
+    },
+    {
+      time: "12:00",
+      name: "Lunch",
+      image: "https://images.unsplash.com/photo-1581091019743-7aaffed1b6d6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJlYWtmYXN0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+      ingredients: "2 eggs, 2 slices of bread, 1 cup of milk",
+    },
+    {
+      time: "18:00",
+      name: "Dinner",
+      image: "https://images.unsplash.com/photo-1581091019743-7aaffed1b6d6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJlYWtmYXN0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+      ingredients: "2 eggs, 2 slices of bread, 1 cup of milk",
+    },
+  ];
+  if (!meals) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      {meals.map((meal, index) => (
+        <div key={index} className="flex items-center justify-between w-full my-4">
+          <div className="text-lg font-bold">{meal.time}</div>
+          <div className="flex items-center">
+            <img src={meal.image} alt={meal.name} className="w-16 h-16 rounded-full" />
+            <div className="ml-4">
+              <h2 className="text-xl font-bold">{meal.name}</h2>
+              <p className="text-sm text-gray-500">{meal.ingredients}</p>
+            </div>
+          </div>
+          <button className="px-4 py-2 bg-blue-500 text-white rounded">Edit</button>
+        </div>
+      ))}
+    </div>
+  );
+};
