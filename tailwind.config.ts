558c0555c35ed1c1e262479497bd0239c2dac4a3
HuberAdrian <huberadrian@web.de>
2023-12-12 03:49:53 +0100

    added color config for tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50:  "#86C232",
+          100: "#61892F",
+          200: "#6B6E70",
+          300: "#222629",
+          400: "#999999",
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
