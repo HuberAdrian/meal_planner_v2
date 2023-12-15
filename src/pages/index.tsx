9c66251388e8830cf17407bdcd768018bc995976
HuberAdrian <huberadrian@web.de>
2023-12-15 19:35:14 +0100

    formatted time display

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,30 @@ import { SignInButton, useUser, SignOutButton } from "@clerk/nextjs";
 import Head from "next/head";
 import Link from "next/link";
 import { Error, Loading } from "~/components/loading";
-
-
 import { api } from "~/utils/api";
+
+
+type Post = {
+  id: string;
+  createdAt: Date;
+  eventDate: Date;
+  eventType: string;
+  authorId: string;
+  topic: string;
+  content: string;
+  completed: boolean;
+};
+
+type GroupedPosts = {
+  [date: string]: Post[];
+};
+
+type DayProps = {
+  date: string;
+  posts: Post[];
+};
+
+
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -15,6 +36,8 @@ export default function Home() {
 
   if (isLoading) return <Loading />;
   if(!data) return <Error />;
+
+  const groupedPosts = groupPostsByDate(data);
 
   return (
     <>
@@ -30,14 +53,10 @@ export default function Home() {
           </div>
           <h2 className="text-4xl text-center py-4">Posts</h2>
           <ul className="px-4 py-2">
-            {data?.map((post) => (
-              <li className="text-[#fff] mb-2" key={post.id}>
-                  {post.content}
-              </li>
+            {Object.entries(groupedPosts).map(([date, posts], index) => (
+              <Day key={index} date={date} posts={posts} />
             ))}
           </ul>
-          <h1 className="text-4xl text-center py-4">Day Component</h1>
-          <Day />
         </div>
       </main>
     </>
@@ -45,42 +64,24 @@ export default function Home() {
 }
 
 
-const Day = (/*{ meals }*/) => {
-  const meals = [
-    {
-      time: "Morgens",
-      name: "Breakfast",
-      image: "https://images.unsplash.com/photo-1581091019743-7aaffed1b6d6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJlYWtmYXN0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
-      ingredients: "2 eggs, 2 slices of bread, 1 cup of milk",
-    },
-    {
-      time: "Mittags",
-      name: "Lunch",
-      image: "https://images.unsplash.com/photo-1581091019743-7aaffed1b6d6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJlYWtmYXN0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
-      ingredients: "2 eggs, 2 slices of bread, 1 cup of milk",
-    },
-    {
-      time: "Abends",
-      name: "Dinner",
-      image: "https://images.unsplash.com/photo-1581091019743-7aaffed1b6d6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJlYWtmYXN0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
-      ingredients: "2 eggs, 2 slices of bread, 1 cup of milk",
-    },
-  ];
-  if (!meals) {
-    return <div>Loading...</div>;
+const Day: React.FC<DayProps> = ({ date, posts }) => {
+  if (!posts) {
+    return <Loading />;
   }
+  // get the date 
+  const formattedDate = new Date(date).toLocaleDateString('de-DE', { weekday: 'long', day: '2-digit', month: '2-digit' });
+  const [weekday, dayMonth] = formattedDate.split(', ');
 
   return (
     <div className="flex flex-col items-center px-4">
-      <h2 className="text-2xl font-bold">Monday</h2>
-      {meals.map((meal, index) => (
+      <h2 className="text-2xl font-bold">{`${weekday}, der ${dayMonth}`}</h2>
+      {posts.map((post, index) => (
         <div key={index} className="flex flex-col sm:flex-row items-center justify-between w-full my-4">
-          <div className="text-lg text-primary-100 font-bold mb-2 sm:mb-0">{meal.time}</div>
+          <div className="text-lg text-primary-100 font-bold mb-2 sm:mb-0">{post.eventType}</div>
           <div className="flex items-center mb-2 sm:mb-0">
-            <img src={meal.image} alt={meal.name} className="w-16 h-16 rounded-full" />
             <div className="ml-4">
-              <h2 className="text-xl font-bold">{meal.name}</h2>
-              <p className="text-sm text-gray-500">{meal.ingredients}</p>
+              <h2 className="text-xl font-bold">{post.topic}</h2>
+              <p className="text-sm text-gray-500">{post.content}</p>
             </div>
           </div>
           <button className="px-4 py-2 bg-blue-500 text-white rounded self-start sm:self-auto">Edit</button>
@@ -89,3 +90,18 @@ const Day = (/*{ meals }*/) => {
     </div>
   );
 };
+
+// This function groups posts by their eventDate, and returns an object where each key is a date and the value is an array of posts for that date
+function groupPostsByDate(posts: Post[]): GroupedPosts {
+return posts.reduce((groupedPosts: GroupedPosts, post) => {
+  const date = post.eventDate.toISOString().split('T')[0];
+  if (date === undefined) {
+    return groupedPosts;
+  }
+  if (!groupedPosts[date]) {
+    groupedPosts[date] = [];
+  }
+  groupedPosts[date]?.push(post);
+  return groupedPosts;
+}, {} as GroupedPosts);
+}
