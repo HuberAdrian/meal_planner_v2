aac2f04ce30fdf08a69444298f145e240ab7f5e8
HuberAdrian <huberadrian@web.de>
2023-12-16 11:23:45 +0100

    changed icons

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -1,4 +1,7 @@
-import { AiFillHome, AiFillCalendar, AiFillHeart, AiFillStar, AiFillSetting } from 'react-icons/ai';
+import { FaCalendar, FaUser} from 'react-icons/fa';
+import { MdLocalGroceryStore } from "react-icons/md";
+import { GiMeal } from "react-icons/gi";
+import { IoIosStats } from "react-icons/io";
 
 type BottomNavBarProps = {
   activePage: string;
@@ -7,11 +10,11 @@ type BottomNavBarProps = {
 const BottomNavBar: React.FC<BottomNavBarProps> = ({ activePage }) => {
   return (
     <div className="fixed bottom-0 w-full bg-gray-100 shadow-md flex items-center justify-around p-4">
-      <AiFillHome className={`text-2xl ${activePage === 'home' ? 'text-blue-500 font-bold' : 'text-gray-500'}`} />
-      <AiFillCalendar className={`text-2xl ${activePage === 'calendar' ? 'text-blue-500 font-bold' : 'text-gray-500'}`} />
-      <AiFillHeart className={`text-2xl ${activePage === 'heart' ? 'text-blue-500 font-bold' : 'text-gray-500'}`} />
-      <AiFillStar className={`text-2xl ${activePage === 'star' ? 'text-blue-500 font-bold' : 'text-gray-500'}`} />
-      <AiFillSetting className={`text-2xl ${activePage === 'setting' ? 'text-blue-500 font-bold' : 'text-gray-500'}`} />
+      <GiMeal className={`text-2xl ${activePage === 'home' ? 'text-blue-500 font-bold' : 'text-gray-500'}`} />
+      <MdLocalGroceryStore className={`text-2xl ${activePage === 'calendar' ? 'text-blue-500 font-bold' : 'text-gray-500'}`} />
+      <FaCalendar className={`text-2xl ${activePage === 'heart' ? 'text-blue-500 font-bold' : 'text-gray-500'}`} />
+      <IoIosStats className={`text-2xl ${activePage === 'star' ? 'text-blue-500 font-bold' : 'text-gray-500'}`} />
+      <FaUser className={`text-2xl ${activePage === 'setting' ? 'text-blue-500 font-bold' : 'text-gray-500'}`} />
     </div>
   );
 };
